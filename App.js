88fb4ae96e624bc10ad1537923a4da945ea69e71
Jennie Vu <jennievu8991@gmail.com>
2023-04-15 19:33:26 -0400

    homescreen, manage medications page create

--- App.js
+++ App.js
@@ -1,12 +1,23 @@
+import { NavigationContainer } from '@react-navigation/native';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { HomeScreen } from './components/HomeScreen';
+import { ManageMedications } from './components/ManageMedications';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
 
+const Stack = createNativeStackNavigator();
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen
+          name="Home"
+          component={HomeScreen}
+          options={{title: 'Welcome'}}
+        />
+        <Stack.Screen name="Manage Medications" component={ManageMedications} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
---
+++ components/ManageMedications.js
@@ -0,0 +1,83 @@
+
+import medications from './medication-data.json';
+import React, { Component } from 'react';
+import { View, Text, StyleSheet, TouchableOpacity, Button, SafeAreaView, ScrollView } from 'react-native';
+
+
+export const ManageMedications = ({ navigation }) => {
+    return (
+        <SafeAreaView style={styles.container}>
+            <ScrollView style={styles.scrollview}>
+                {medications.medications.map(medication => (
+                    <View key={medication.name} style={styles.medication}>
+                        <Text style={styles.medicationName}>{medication.name}</Text>
+                        <View style={styles.medicationSchedule}>
+                            {medication.schedule.map(dose => (
+                                <Text key={dose.time} style={styles.dose}>
+                                    {dose.time}: {dose.quantity} {dose.unit}
+                                </Text>
+                            ))}
+                        </View>
+                        <TouchableOpacity style={styles.deleteButton}>
+                            <Text style={styles.deleteButtonText}>Delete</Text>
+                        </TouchableOpacity>
+                    </View>
+                ))}
+                <TouchableOpacity style={styles.button}>
+                    <Text style={styles.buttonText}>Add Medication</Text>
+                </TouchableOpacity>
+            </ScrollView>
+        </SafeAreaView>
+    );
+};
+
+
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+        paddingHorizontal: 20
+    },
+    scrollview: {
+        marginHorizontal: 20,
+        width: '80%',
+        alignContent: 'center'
+    },
+    medication: {
+        marginBottom: 20
+    },
+    medicationName: {
+        fontSize: 18,
+        fontWeight: 'bold',
+        marginBottom: 10
+    },
+    medicationSchedule: {
+        marginLeft: 20
+    },
+    dose: {
+        fontSize: 14,
+        marginBottom: 2
+    },
+    button: {
+        backgroundColor: '#007AFF',
+        padding: 10,
+        borderRadius: 5,
+        marginTop: 10
+    },
+    buttonText: {
+        color: '#fff',
+        fontSize: 16,
+        fontWeight: 'bold',
+        textAlign: 'center'
+    },
+    deleteButton: {
+        backgroundColor: '#00000000'
+    },
+    deleteButtonText: {
+        color: '#5A5A5A',
+        textDecorationLine: 'underline'
+    }
+});
