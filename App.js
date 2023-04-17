436dec3be5e7162ad3fddf373f44a3158d10d15b
Ernesto <122322979+erntinall@users.noreply.github.com>
2023-04-17 13:17:37 -0400

    Update to contain LogScreen

--- App.js
+++ App.js
@@ -1,8 +1,9 @@
 import { NavigationContainer } from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import MainMenu from './components/MainMenu';
-import ManageMedications from './components/ManageMedications';
-import MyData from './components/MyData';
+import ManageMedications from './components/ManageMedications'
+import MyData from './components/MyData'
+import LogScreen from './components/LogScreen'
 
 const Stack = createNativeStackNavigator();
 export default function App() {
@@ -12,6 +13,7 @@ export default function App() {
         <Stack.Screen name="MainMenu" component={MainMenu} options={{title: 'Welcome'}}/>
         <Stack.Screen name="Manage Medications" component={ManageMedications}/>
         <Stack.Screen name="My Data" component={MyData}/>
+        <Stack.Screen name="LogScreen" component={LogScreen}/>
       </Stack.Navigator>
     </NavigationContainer>
   );
