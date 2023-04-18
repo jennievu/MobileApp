cab7371245e3303fad74edb3fff8a1bcddcf5530
Ernesto <122322979+erntinall@users.noreply.github.com>
2023-04-18 11:56:32 -0400

    Add LogScreen data to stack

--- App.js
+++ App.js
@@ -3,6 +3,7 @@ import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import MainMenu from './components/MainMenu';
 import ManageMedications from './components/ManageMedications';
 import MyData from './components/MyData';
+import LogScreen from './components/LogScreen';
 
 const Stack = createNativeStackNavigator();
 export default function App() {
@@ -12,6 +13,7 @@ export default function App() {
         <Stack.Screen name="MainMenu" component={MainMenu} options={{title: 'Welcome'}}/>
         <Stack.Screen name="Manage Medications" component={ManageMedications}/>
         <Stack.Screen name="My Data" component={MyData}/>
+        <Stack.Screen name="LogScreen" component={LogScreen}/>
       </Stack.Navigator>
     </NavigationContainer>
   );
