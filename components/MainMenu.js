8c57d687501bad5ba417e41f8ca33d92613fd832
Ernesto <122322979+erntinall@users.noreply.github.com>
2023-04-18 11:58:40 -0400

    Update MainMenu with LogScreen plus button.
    Added a plus button to indicate and start LogScreen.js

--- components/MainMenu.js
+++ components/MainMenu.js
@@ -9,15 +9,22 @@ class MainMenu extends Component {
         return (
             <View style={styles.container}>
                 <Text style={styles.title}>Main Menu</Text>
+
                 <TouchableOpacity
                     style={styles.button}
                     onPress={() => this.props.navigation.navigate('Manage Medications')}
                 >
                     <Text style={styles.buttonText}>Manage Medication</Text>
                 </TouchableOpacity>
+
                 <TouchableOpacity style={styles.button} onPress={() => this.props.navigation.navigate('My Data')}>
                     <Text style={styles.buttonText}>My Data</Text>
                 </TouchableOpacity>
+
+                <TouchableOpacity onPress={() => this.props.navigation.navigate('LogScreen')} style={{ position: 'absolute', bottom: 20, right: 20 }}>
+                  <Text style={{ fontSize: 30, color: 'white', backgroundColor: 'blue', padding: 10, borderRadius: 50 }}>+</Text>
+                </TouchableOpacity>
+
             </View>
         );
     }
